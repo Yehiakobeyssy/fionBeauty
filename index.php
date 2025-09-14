c6a4bb0c071a0781000ea3f3d6c553029334714a
yehia kobeyssy <yehiakobeyssy2018@gmail.com>
2025-09-14 21:45:54 +0200

    make slideshow in index.php

--- index.php
+++ index.php
@@ -18,7 +18,25 @@
         include 'include/header.php';
         include 'include/clientheader.php'; 
         include 'include/catecorysname.php';
+    
+        $sql = "SELECT slideScr, slideHref 
+                FROM tblslideshow 
+                WHERE pageSide='index.php' AND slideActive=1 
+                ORDER BY slideID ASC";
+        $stmt = $con->prepare($sql);
+        $stmt->execute();
+        $slides = $stmt->fetchAll(PDO::FETCH_ASSOC);
     ?>
+
+    <div class="slideshow-container">
+        <?php foreach ($slides as $slide): ?>
+            <div class="mySlide">
+                <a href="<?php echo htmlspecialchars($slide['slideHref']); ?>">
+                    <img src="images/slide/<?php echo htmlspecialchars($slide['slideScr']); ?>" alt="Slide">
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
     <?php include 'common/jslinks.php'?>
     <script src="js/index.js"></script>
 </body>
---
+++ js/index.js
@@ -0,0 +1,15 @@
+ $(document).ready(function() {
+    let slideIndex = 0;
+    let slides = $(".mySlide");
+    let total = slides.length;
+
+    function showSlides() {
+        slides.hide();
+        slideIndex++;
+        if (slideIndex > total) { slideIndex = 1; }
+        slides.eq(slideIndex - 1).fadeIn();
+        setTimeout(showSlides, 5000); // 5 seconds per slide
+    }
+
+    showSlides();
+});
